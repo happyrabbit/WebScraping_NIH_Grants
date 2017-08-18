45051d3acb97eeda45a63b1a6d22871ea0161d6e
Hui Lin <longqiman@gmail.com>
2017-08-18 16:52:03 -0500

    NIH_WEB CODE

--- NIH_WEB.R
+++ NIH_WEB.R
@@ -13,14 +13,39 @@ links<-getHTMLLinks(url)
 # get all links with abstract needed
 links<-links[grep("https://projectreporter.nih.gov/project_info_description.cfm?", links)]
 
-## step 2: fetch personal wiki pages
+## step 2: fetch project abstract pages
+
 HTML <- list()
 # loop
-i<-1
 for ( i in seq_along(links) ){
   # url
   url <- links[i]
    # read in files
   HTML[[i]] <- read_html(url)
 }
+######################
+res <- NULL
+for (i in 1:length(HTML)){
+tables<-html_table(HTML[[i]])
+## get titles
+title <- tables[[1]]
+title2 <- title[1,]
+names(title2) <- c("ProjectNumber", "Title","PI", "AwardeeOrganization")
+title2[1,1]<- title[1,2]
+title2[1,2]<- title[2,2]
+title2[1,3]<- title[1,4]
+title2[1,4]<- title[1,4]
 
+## get abstract
+abstract <- tables[[2]]
+abstract2 = title2[1,1:3]
+names(abstract2)<- c("Abstract_Text","Public_Health_Relevance_Statemen","Project_Terms")
+abstract2[1,1]<-abstract[1,1]
+abstract2[1,2]<-abstract[3,1]
+abstract2[1,3]<-abstract[5,1]
+
+res0 <- data.frame(c(title2, abstract2))
+res<-rbind(res,res0)
+}
+
+write.csv(res, "Data/AbstractTable.csv", row.names=F)
